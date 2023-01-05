4aa84460ab377ad888cd7bf784fea7b5e5d8c61c
krishna <bhattakrishna111@gmail.com>
2023-01-05 10:32:39 +0545

    creating post feature completed

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Grid, CircularProgress } from '@material-ui/core';
+
 
 import Post from './Post/Post';
 import useStyles from "./styles";
@@ -9,12 +11,17 @@ const Posts = () => {
 
   const posts = useSelector((state) => state.posts);
   return (
-    <>
-      <div>Posts</div>
-      <Post />
-      <Post />
-    </>
+     !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6} md={6}>
+            <Post post={post} />
+          </Grid>
+        ))}
+      </Grid>
+    )
   );
+  
 }
 
 export default Posts
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 import { createRoot } from "react-dom/client";
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
